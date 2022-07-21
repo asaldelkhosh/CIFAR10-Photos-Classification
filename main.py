9f3a9014cac4c4d7ac4b45584b63feda596bcd21
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-21 18:49:14 +0430

    add: loading dataset

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 # example of loading the cifar10 dataset
 from matplotlib import pyplot
 from keras.datasets import cifar10
+
+
+
+if __name__ == "__main__":
+    # load dataset
+    (trainX, trainy), (testX, testy) = cifar10.load_data()
+
+    # summarize loaded dataset
+    print('Train: X=%s, y=%s' % (trainX.shape, trainy.shape))
+    print('Test: X=%s, y=%s' % (testX.shape, testy.shape))
+
+    # plot first few images
+    for i in range(9):
+        # define subplot
+        pyplot.subplot(330 + 1 + i)
+        # plot raw pixel data
+        pyplot.imshow(trainX[i])
+
+    # show the figure
+    pyplot.show()
+    
