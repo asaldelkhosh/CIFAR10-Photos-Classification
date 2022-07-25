0920de77959ae6bd5325fdc88daaeebda08785b8
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-25 09:33:18 +0430

    add: full model

--- model.py
+++ model.py
@@ -9,13 +9,18 @@ from keras.optimizers import SGD
 
 
 
+MODEL = 'final_model.h5'
+
+
 # load train and test dataset
 def load_dataset():
 	# load dataset
 	(trainX, trainY), (testX, testY) = cifar10.load_data()
+
 	# one hot encode target values
 	trainY = to_categorical(trainY)
 	testY = to_categorical(testY)
+
 	return trainX, trainY, testX, testY
  
 
@@ -24,9 +29,11 @@ def prep_pixels(train, test):
 	# convert from integers to floats
 	train_norm = train.astype('float32')
 	test_norm = test.astype('float32')
+
 	# normalize to range 0-1
 	train_norm = train_norm / 255.0
 	test_norm = test_norm / 255.0
+
 	# return normalized images
 	return train_norm, test_norm
 
@@ -46,9 +53,11 @@ def define_model():
 	model.add(Flatten())
 	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
 	model.add(Dense(10, activation='softmax'))
+
 	# compile model
 	opt = SGD(lr=0.001, momentum=0.9)
 	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    
 	return model
 
 
@@ -63,4 +72,4 @@ def run_test_harness():
 	# fit model
 	model.fit(trainX, trainY, epochs=100, batch_size=64, verbose=0)
 	# save model
-	model.save('final_model.h5')
+	model.save(MODEL)
