d3ba4883c4a9e90dafc0cb12671135e153c50157
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-25 09:38:50 +0430

    with: dataset module

--- main.py
+++ main.py
@@ -1,126 +1,16 @@
-# example of loading the cifar10 dataset
-import sys
-from matplotlib import pyplot
-from keras.datasets import cifar10
-from keras.utils import to_categorical
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.optimizers import SGD
-from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
+from dataset import load_dataset, prep_pixels
 
 
 
-# load train and test dataset
-def load_dataset():
+# run the test harness for evaluating a model
+def run_test_harness():
 	# load dataset
-	(trainX, trainY), (testX, testY) = cifar10.load_data()
-
-	# one hot encode target values
-	trainY = to_categorical(trainY)
-	testY = to_categorical(testY)
-
-	return trainX, trainY, testX, testY
-
-
-# scale pixels
-def prep_pixels(train, test):
-	# convert from integers to floats
-	train_norm = train.astype('float32')
-	test_norm = test.astype('float32')
-
-	# normalize to range 0-1
-	train_norm = train_norm / 255.0
-	test_norm = test_norm / 255.0
-    
-	# return normalized images
-	return train_norm, test_norm
-
-
-# define cnn model
-def define_model():
-	model = Sequential()
-	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
-	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Dropout(0.2))
-	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Dropout(0.2))
-	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Dropout(0.2))
-	model.add(Flatten())
-	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-	model.add(Dropout(0.2))
-	model.add(Dense(10, activation='softmax'))
-	# compile model
-	opt = SGD(lr=0.001, momentum=0.9)
-	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-	return model
-
-
-# plot diagnostic learning curves
-def summarize_diagnostics(history):
-	# plot loss
-	pyplot.subplot(211)
-	pyplot.title('Cross Entropy Loss')
-	pyplot.plot(history.history['loss'], color='blue', label='train')
-	pyplot.plot(history.history['val_loss'], color='orange', label='test')
-	# plot accuracy
-	pyplot.subplot(212)
-	pyplot.title('Classification Accuracy')
-	pyplot.plot(history.history['accuracy'], color='blue', label='train')
-	pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
-	# save plot to file
-	filename = sys.argv[0].split('/')[-1]
-	pyplot.savefig(filename + '_plot.png')
-	pyplot.close()
-
-
-if __name__ == "__main__":
-    # load dataset
-    trainX, trainy, testX, testy = load_dataset()
-
+	trainX, trainY, testX, testY = load_dataset()
 	# prepare pixel data
 	trainX, testX = prep_pixels(trainX, testX)
-
-    # summarize loaded dataset
-    print('Train: X=%s, y=%s' % (trainX.shape, trainy.shape))
-    print('Test: X=%s, y=%s' % (testX.shape, testy.shape))
-
-    # plot first few images
-    for i in range(9):
-        # define subplot
-        pyplot.subplot(330 + 1 + i)
-        # plot raw pixel data
-        pyplot.imshow(trainX[i])
-
-    # show the figure
-    pyplot.show()
-
-	# create model
-	model = define_model()
-
-	# create data generator
-	datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
-	# prepare iterator
-	it_train = datagen.flow(trainX, trainY, batch_size=64)
-
-	# fit model
-	steps = int(trainX.shape[0] / 64)
-	history = model.fit(it_train, steps_per_epoch=steps, epochs=100, validation_data=(testX, testY), verbose=0)
-
-	# save model
-	model.save('final_model.h5')
-
-	# evaluate model
+	# load model
+	model = load_model('final_model.h5')
+	# evaluate model on test dataset
 	_, acc = model.evaluate(testX, testY, verbose=0)
-
-	# summarizing the diagnostics
-	summarize_diagnostics(history)
 	print('> %.3f' % (acc * 100.0))
--- model.py
+++ model.py
@@ -1,5 +1,4 @@
-from keras.datasets import cifar10
-from keras.utils import to_categorical
+from dataset import load_dataset, prep_pixels
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
@@ -10,32 +9,6 @@ from keras.optimizers import SGD
 
 
 MODEL = 'final_model.h5'
-
-
-# load train and test dataset
-def load_dataset():
-	# load dataset
-	(trainX, trainY), (testX, testY) = cifar10.load_data()
-
-	# one hot encode target values
-	trainY = to_categorical(trainY)
-	testY = to_categorical(testY)
-
-	return trainX, trainY, testX, testY
- 
-
-# scale pixels
-def prep_pixels(train, test):
-	# convert from integers to floats
-	train_norm = train.astype('float32')
-	test_norm = test.astype('float32')
-
-	# normalize to range 0-1
-	train_norm = train_norm / 255.0
-	test_norm = test_norm / 255.0
-
-	# return normalized images
-	return train_norm, test_norm
 
 
 # define cnn model
@@ -57,7 +30,7 @@ def define_model():
 	# compile model
 	opt = SGD(lr=0.001, momentum=0.9)
 	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-    
+
 	return model
 
 
@@ -73,3 +46,7 @@ def run_test_harness():
 	model.fit(trainX, trainY, epochs=100, batch_size=64, verbose=0)
 	# save model
 	model.save(MODEL)
+
+
+if __name__ == "__main__":
+    run_test_harness()
