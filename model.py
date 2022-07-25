80a4a5dce0ab91438a7395405dd41ff56402401b
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-25 09:42:50 +0430

    update: model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from dataset import load_dataset, prep_pixels
+from matplotlib import pyplot
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
@@ -15,23 +16,50 @@ MODEL = 'final_model.h5'
 def define_model():
 	model = Sequential()
 	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+	model.add(BatchNormalization())
 	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
 	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.2))
 	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
 	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
 	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.3))
 	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
 	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
 	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.4))
 	model.add(Flatten())
 	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(BatchNormalization())
+	model.add(Dropout(0.5))
 	model.add(Dense(10, activation='softmax'))
-
 	# compile model
 	opt = SGD(lr=0.001, momentum=0.9)
 	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-
 	return model
+
+
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+	# plot loss
+	pyplot.subplot(211)
+	pyplot.title('Cross Entropy Loss')
+	pyplot.plot(history.history['loss'], color='blue', label='train')
+	pyplot.plot(history.history['val_loss'], color='orange', label='test')
+	# plot accuracy
+	pyplot.subplot(212)
+	pyplot.title('Classification Accuracy')
+	pyplot.plot(history.history['accuracy'], color='blue', label='train')
+	pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+	# save plot to file
+	filename = sys.argv[0].split('/')[-1]
+	pyplot.savefig(filename + '_plot.png')
+	pyplot.close()
 
 
 # run the test harness for evaluating a model
@@ -42,10 +70,17 @@ def run_test_harness():
 	trainX, testX = prep_pixels(trainX, testX)
 	# define model
 	model = define_model()
+	# create data generator
+	datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+	# prepare iterator
+	it_train = datagen.flow(trainX, trainY, batch_size=64)
 	# fit model
-	model.fit(trainX, trainY, epochs=100, batch_size=64, verbose=0)
+	steps = int(trainX.shape[0] / 64)
+	history = model.fit_generator(it_train, steps_per_epoch=steps, epochs=400, validation_data=(testX, testY), verbose=0)
 	# save model
 	model.save(MODEL)
+	# learning curves
+	summarize_diagnostics(history)
 
 
 if __name__ == "__main__":
